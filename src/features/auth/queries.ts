9f9cc0e918c375280b636123c615e6dfdb60b0a2
Adle25 <askaradilet41@gmail.com>
2024-10-29 19:37:05 +0500

    fixing error states

--- src/features/auth/queries.ts
+++ src/features/auth/queries.ts
@@ -1,8 +1,5 @@
 "use server";
 
-import { Account, Client } from "node-appwrite";
-import { cookies } from "next/headers";
-import { AUTH_COOKIE } from "./constants";
 import { createSessionClient } from "@/lib/appwrite";
 
 
