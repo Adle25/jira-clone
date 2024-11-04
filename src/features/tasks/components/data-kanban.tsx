6f74812a5de43f39958a9638c7dcf77310fd3b11
Adle25 <askaradilet41@gmail.com>
2024-11-04 21:06:01 +0500

    added kanban data card

--- src/features/tasks/components/data-kanban.tsx
+++ src/features/tasks/components/data-kanban.tsx
@@ -2,6 +2,7 @@ import { Task, TaskStatus } from "../types";
 import { DragDropContext, Droppable, Draggable, DropResult } from "@hello-pangea/dnd";
 import React, { useCallback, useEffect, useState } from "react";
 import { KanbanColumnHeader } from "./kanban-column-header";
+import { KanbanCard } from "./kanban-card";
 
 const boards: TaskStatus[] = [
     TaskStatus.BACKLOG,
@@ -47,6 +48,34 @@ export const DataKanban = ({ data }: DataKanbanProps) => {
                     return (
                         <div key={board} className="flex-1 mx-2 bg-muted p-1.5 rounded-md min-w-[200px]">
                             <KanbanColumnHeader board={board} taskCount={tasks[board].length} />
+                            <Droppable droppableId={board}>
+                                {(provided) => (
+                                    <div
+                                        {...provided.droppableProps}
+                                        ref={provided.innerRef}
+                                        className="min-h-[200px] py-1.5"
+                                    >
+                                        {tasks[board].map((task, index) => (
+                                            <Draggable
+                                                key={task.$id}
+                                                draggableId={task.$id}
+                                                index={index}
+                                            >
+                                                {(provided) => (
+                                                    <div
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}
+                                                        ref={provided.innerRef}
+                                                    >
+                                                        <KanbanCard task={task} />
+                                                    </div>
+                                                )}
+                                            </Draggable>
+                                        ))}
+                                        {provided.placeholder}
+                                    </div>
+                                )}
+                            </Droppable>
                         </div>
                     )
                 })}
