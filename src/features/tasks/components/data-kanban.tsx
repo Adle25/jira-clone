6be7b7d64d9e925c92f2c0a8fc8c6f378fcd3e77
Adle25 <askaradilet41@gmail.com>
2024-11-05 21:01:00 +0500

    finished data kanban

--- src/features/tasks/components/data-kanban.tsx
+++ src/features/tasks/components/data-kanban.tsx
@@ -18,9 +18,10 @@ type TasksState = {
 
 interface DataKanbanProps {
     data: Task[];
+    onChange: (tasks: { $id: string; status: TaskStatus; position: number }[]) => void;
 };
 
-export const DataKanban = ({ data }: DataKanbanProps) => {
+export const DataKanban = ({ data, onChange }: DataKanbanProps) => {
     const [tasks, setTasks] = useState<TasksState>(() => {
         const initialTasks: TasksState = {
             [TaskStatus.BACKLOG]: [],
@@ -41,8 +42,100 @@ export const DataKanban = ({ data }: DataKanbanProps) => {
         return initialTasks;
     });
 
+    useEffect(() => {
+        const newTasks: TasksState = {
+            [TaskStatus.BACKLOG]: [],
+            [TaskStatus.TODO]: [],
+            [TaskStatus.IN_PROGRESS]: [],
+            [TaskStatus.IN_REVIEW]: [],
+            [TaskStatus.DONE]: [],
+        };
+
+        data.forEach((task) => {
+            newTasks[task.status].push(task);
+        });
+
+        Object.keys(newTasks).forEach((status) => {
+            newTasks[status as TaskStatus].sort((a, b) => a.position - b.position);
+        });
+
+        setTasks(newTasks);
+
+    }, [data]);
+
+    const onDragEnd = useCallback((result: DropResult) => {
+        if (!result.destination) return;
+
+        const { source, destination } = result;
+        const sourceStatus = source.droppableId as TaskStatus;
+        const destinationStatus = destination.droppableId as TaskStatus;
+
+        let updatesPayload: { $id: string; status: TaskStatus; position: number; }[] = [];
+
+        setTasks((prevTasks) => {
+            const newTasks = { ...prevTasks };
+            const sourceColumn = [...newTasks[sourceStatus]];
+            const [movedTask] = sourceColumn.splice(source.index, 1);
+
+            if (!movedTask) {
+                console.error("No task found at the source index");
+                return prevTasks;
+            }
+
+            const updatedMovedTask = sourceStatus !== destinationStatus ? { ...movedTask, status: destinationStatus } : movedTask;
+
+            newTasks[sourceStatus] = sourceColumn;
+
+            const destinationColumn = [...newTasks[destinationStatus]];
+            destinationColumn.splice(destination.index, 0, updatedMovedTask);
+            newTasks[destinationStatus] = destinationColumn;
+
+            updatesPayload = [];
+
+            updatesPayload.push({
+                $id: updatedMovedTask.$id,
+                status: destinationStatus,
+                position: Math.min((destination.index + 1) * 1000, 1_000_000)
+            });
+
+            newTasks[destinationStatus].forEach((task, index) => {
+                if (task && task.$id !== updatedMovedTask.$id) {
+                    const newPosition = Math.min((index + 1) * 1000, 1_000_000);
+
+                    if (task.position !== newPosition) {
+                        updatesPayload.push({
+                            $id: task.$id,
+                            status: destinationStatus,
+                            position: newPosition
+                        });
+                    }
+                }
+            });
+
+            if (sourceStatus !== destinationStatus) {
+                newTasks[sourceStatus].forEach((task, index) => {
+                    if (task) {
+                        const newPosition = Math.min((index + 1) * 1000, 1_000_000);
+
+                        if (task.position !== newPosition) {
+                            updatesPayload.push({
+                                $id: task.$id,
+                                status: sourceStatus,
+                                position: newPosition
+                            });
+                        }
+                    }
+                });
+            }
+
+            return newTasks;
+        });
+
+        onChange(updatesPayload);
+    }, [onChange]);
+
     return (
-        <DragDropContext onDragEnd={() => { }}>
+        <DragDropContext onDragEnd={onDragEnd}>
             <div className="flex overflow-x-auto">
                 {boards.map((board) => {
                     return (
