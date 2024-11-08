61d8bd8577af6ec6b5c7fefc5b19d6316bac6bbb
Adle25 <askaradilet41@gmail.com>
2024-11-08 22:32:49 +0500

    finished refactoring server components

--- src/features/projects/components/edit-project-form.tsx
+++ src/features/projects/components/edit-project-form.tsx
@@ -54,11 +54,7 @@ export const EditProjectForm = ({ onCancel, initialValues }: EditProjectFormProp
             image: values.image instanceof File ? values.image : "",
         };
 
-        mutate({ form: finalValues, param: { projectId: initialValues.$id } }, {
-            onSuccess: ({ data }) => {
-                form.reset();
-            }
-        });
+        mutate({ form: finalValues, param: { projectId: initialValues.$id } });
     };
 
     const handleDelete = async () => {
--- src/features/workspaces/components/edit-workspace-form.tsx
+++ src/features/workspaces/components/edit-workspace-form.tsx
@@ -62,11 +62,7 @@ export const EditWorkspaceForm = ({ onCancel, initialValues }: EditWorkspaceForm
             image: values.image instanceof File ? values.image : "",
         };
 
-        mutate({ form: finalValues, param: { workspaceId: initialValues.$id } }, {
-            onSuccess: ({ data }) => {
-                form.reset();
-            }
-        });
+        mutate({ form: finalValues, param: { workspaceId: initialValues.$id } });
     };
 
     const handleDelete = async () => {
